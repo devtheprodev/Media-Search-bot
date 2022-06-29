1bc80669004520b72ddeeb17a27ac2abd1a98be6
slr <96696830+devtheprodev@users.noreply.github.com>
2022-06-29 15:41:35 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -75,7 +75,14 @@ async def answer(bot, query):
         )
 
 
-
+def get_reply_markup(username, query):
+    url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
+    buttons = [
+        [
+            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+        ]
+    ]
+    return InlineKeyboardMarkup(buttons)
 
 
 def size_formatter(size):
